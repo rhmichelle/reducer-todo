f4a65abc6b575044bdf9c1ae5cc36098b45ca1d9
michelleRH <dev@michellerh.com>
2021-02-26 03:23:25 -0500

    Added handleChanges

--- todo/src/Components/Form.js
+++ todo/src/Components/Form.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+import reducer from '../Reducer/reducer';
+import actions from '../Reducer/Actions';
+
+
 
 const Form = () => {
+    const initialState = 
+        {
+            item: 'Fill out Happy Planner',
+            completed: false,
+            id: 1
+        }
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleChanges = (event) => {
+        dispatch(actions.TODO_ADD(event.target.value));
+    }
+    
+
     return(
         <form>
             <label htmlFor='title'>Title</label>
@@ -10,6 +28,14 @@ const Form = () => {
                 placeholder='Enter Title of Task'
                 // onChange={handleChanges}
             />
+        {state.map(todo => (
+            <div>
+                <p>Task: {todo.item}</p>
+                <p>completed: {todo.completed}</p>
+            </div>
+            ))}
+            <button onClick={handleChanges}>Add</button>
+            
         </form>
     );
 }
