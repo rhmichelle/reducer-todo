77fc7905faa584d8b0d058c579a8d525c30cbf9f
michelleRH <dev@michellerh.com>
2021-02-25 22:06:04 -0500

    Updated README. Created Reducer and initialState

---
+++ todo/src/Reducer/reducer.js
@@ -0,0 +1,34 @@
+const initialState = [
+    {
+        item: 'Fill out Happy Planner',
+        completed: false,
+        id: 1
+    },
+    {
+        item: 'Go to bed early',
+        completed: false,
+        id: 2    
+    },
+    {
+        item: 'Bake Cookies',
+        completed: false,
+        id: 3
+    },
+    {
+        item: 'Start Social Media',
+        completed: false,
+        id: 4
+    }
+]
+
+
+
+const reducer = (state, action) => {
+    switch(action.type) {
+
+    }
+    return;
+}
+
+export default { initialState, reducer }
+
